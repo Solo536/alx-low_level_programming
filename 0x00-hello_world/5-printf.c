7d072350db6a4922c38a96272cb8b02ca85c7846
Solo536 <solomonadjei536@gmail.com>
2023-03-16 19:53:47 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if Success.
+ */
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
